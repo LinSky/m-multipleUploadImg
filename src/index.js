d297602637eb7aeaf04a10977a9014e97b0b146f
LinSky <397588184@qq.com>
2018-05-22 08:59:30 +0800

    inputChangeHandle and readFiles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 class MultipleUploader {
-    constructor(options) {
-        this.opts = options
+    constructor (options) {
+        let defaultOpts = {
+            el: null
+        }
+        this.opts = Object.assign({}, defaultOpts, options)
         this.input = document.getElementById(this.opts.el)
         this.init()
     }
 
     init () {
         let _this = this
-        this.input.addEventListener('change', this.inputChangeHandle, false);
+        this.input.addEventListener('change', this.inputChangeHandle.bind(this), false);
     }
 
     inputChangeHandle (event) {
-        console.log(Array.prototype.slice.call(event.target.files));
+        var _this = this
+        console.log(_this);
+        // console.log(Array.prototype.slice.call(event.target.files));
+        _this.readFiles(event.target.files)
+    }
+
+    readFiles (files) {
+        console.log(123);
+        let fd = new FormData()
+        if (files.length > 0) {
+            for (var i = 0; i < files.length; i++) {
+                fd.append('files', files[i])
+            }
+            let xhr = new XMLHttpRequest()
+            xhr.open('POST', 'http://localhost:3000/image-upload')
+            xhr.onreadystatechange = function(){
+                if (xhr.readyState== 4&& xhr.status== 200){
+                    console.log('上传成功'+xhr.responseText);
+                }
+            };
+            xhr.send(fd)
+        }
     }
 }
 
